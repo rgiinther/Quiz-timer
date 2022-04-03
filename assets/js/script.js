1f7b1eb7af6b61cbbc3bc25290dc0812f8b7d38b
rgiinther <r.giin95@gmail.com>
2022-04-02 23:00:57 -0500

    made questions into array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,106 +1,94 @@
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-    function showQuestions(questions, quizContainer){
-        //code will go here
-    }
-
-    function showResults(questions, quizContainer, resultsContainer){
-        //code will go here
-    }
-
-    //show the questions
-    showQuestions(questions, quizContainer);
-
-    //when use clicks submit, show results
-    submitButton.onclick = function(){
-        showResults(questions, quizContainer, resultsContainer);
-    }
-}
 //quiz questions
-
-var myQuestions = [
+var currentQuestionIndex = 0
+var myQuestions= [
     {
         question: "What is the name of the artist who painted Mona Lisa?", 
-        answers: {
-            a: 'Van Gogh',
-            b: 'Picasso',
-            c: 'Leonardo DaVinci'
-        },
-         correctAnswer: 'c'
+        answers: ['Van Gogh', 'Picasso', 'Leonardo Davinci'],
+         correctAnswer: 'Leonardo Davinci'
     },
     {
         question: "What is the capital of Brazil?",
-        answers: {
-            a: 'Brasilia',
-            b: 'Lima',
-            c: 'Frankfurt'
-        },
-        correctAnswer: 'a'
+        answers: ['Brasilia', 'Lima', 'Frankfurt'],
+        correctAnswer: 'Brasilia'
     },
     {
         question: "Where is Lincoln Memorial situated in America?",
-        answers: {
-            a: 'Mount Vernon',
-            b: 'Washington D.C.',
-            c: 'Los Angeles'
-        },
-        correctAnswer: 'b'  
+        answers: ['Mount Vernon', 'Washington D.C.', 'Los Angeles'],
+        correctAnswer: 'Washington D.C.'  
     },
     {
         question: "What is the capital of Geremany?",
-        answers: {
-            a: 'Berlin',
-            b: 'Munich',
-            c: 'Frankfurt'
-        },
-        correctAnswer: 'a'
+        answers: ['Berlin', 'Munich', 'Frankfurt'],
+        correctAnswer: 'Berlin'
     },   
     {
         question: "What temperature does water boil at?",
-        answers: {
-            a: '30 C',
-            b: '100 C',
-            c: '212 C'
-        },
-        correctAnswer: 'b'
+        answers: ['32 C', '100 C', '212 C'],
+        correctAnswer: '100 C'
     },
     {
         question: "Which country is famous for tulips?",
-        answers: {
-            a: 'Netherlands',
-            b: 'France',
-            c: 'Norway'
-        },
-        correctAnswer: 'a'
+        answers: ['Netherlands', 'France', 'Norway'],
+        correctAnswer: 'Netherlands'
     },
     {
         question: "Which planet is closest to the sun?",
-        answers: {
-            a: 'Saturn',
-            b: 'Mars',
-            c: 'Mercury'
-        },
-        correctAnswer: 'b'
+        answers: ['Saturn', 'Mars', 'Mercury'],
+        correctAnswer: 'Mercury'
     },
     {
         question: "Who was the first president of the United States",
-        answers: {
-            a: 'Washington',
-            b: 'Lincoln',
-            c: 'Hamilton'
-        },
-        correctAnswer: 'a'
+        answers: ['Washington', 'Lincoln', 'Hamilton'],
+        correctAnswer: 'Washington'
     },
 
     {
         question: "What is the capital of Guatemala",
-        answers: {
-            a: 'Guatemala City',
-            b: 'Antigua',
-            c: 'Quetzaltenango'
-        },
-        correctAnswer: 'a'
+        answers: ['Guatemala City', 'Antigua', 'Quetzaltenango'],
+        correctAnswer: 'Guatemala City'
     },
 
 ];
+//declare / create variable to hold HTML reference
 
+
+//Event listener for button
+.addeventlistener()
+
+//function to select question     
+function showQuestions(){
+    var quizQuestion = myQuestions [currentQuestionIndex]
+    var questionTitle = document.querySelector('.title')
+    questionTitle.textContent = quizQuestion.question
+
+    // for each question...
+    for(var i=0; i<myQuestions.length; i++);
+    
+};
+
+
+function generateQuiz(){
+    showQuestions();
+    startButton.onclick = function(){
+      
+    }
+
+		
+		/*// first reset the list of answers
+		answers = [];
+
+		// for each available answer to this question...
+		for(letter in questions[i].answers){
+
+			
+
+		// add this question and its answers to the output
+		output.push(
+			'<div class="question">' + questions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
+	}*/
+
+	// Combine output list and HTML
+	//quizContainer.innerHTML = output.join('');
+ //showQuestions(questions, quizContainer);

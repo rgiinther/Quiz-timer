11f9d4ff237e311fa3741b8a03070aed3f7d006c
rgiinther <r.giin95@gmail.com>
2022-05-20 18:28:52 -0500

    updated index and fixed timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,94 +1,102 @@
+//quiz w/ start page, button that starts quiz, 
+//questions to display after start
+//questions will display text and answers w/ button 
+//function that takes to next question after rprevious respose
+
+//set attribute, create element, append child 
+
+
 //quiz questions
 var currentQuestionIndex = 0
 var myQuestions= [
     {
         question: "What is the name of the artist who painted Mona Lisa?", 
-        answers: ['Van Gogh', 'Picasso', 'Leonardo Davinci'],
-         correctAnswer: 'Leonardo Davinci'
+        choices: ['Van Gogh', 'Picasso', 'Leonardo Davinci'],
+        answer: 'Leonardo Davinci'
     },
     {
         question: "What is the capital of Brazil?",
-        answers: ['Brasilia', 'Lima', 'Frankfurt'],
-        correctAnswer: 'Brasilia'
+        choices: ['Brasilia', 'Lima', 'Frankfurt'],
+        answer: 'Brasilia'
     },
     {
         question: "Where is Lincoln Memorial situated in America?",
-        answers: ['Mount Vernon', 'Washington D.C.', 'Los Angeles'],
-        correctAnswer: 'Washington D.C.'  
+        choices: ['Mount Vernon', 'Washington D.C.', 'Los Angeles'],
+        answer: 'Washington D.C.'  
     },
     {
         question: "What is the capital of Geremany?",
-        answers: ['Berlin', 'Munich', 'Frankfurt'],
-        correctAnswer: 'Berlin'
+        choices: ['Berlin', 'Munich', 'Frankfurt'],
+        answer: 'Berlin'
     },   
     {
         question: "What temperature does water boil at?",
-        answers: ['32 C', '100 C', '212 C'],
-        correctAnswer: '100 C'
+        choices: ['32 C', '100 C', '212 C'],
+        answer: '100 C'
     },
     {
         question: "Which country is famous for tulips?",
-        answers: ['Netherlands', 'France', 'Norway'],
-        correctAnswer: 'Netherlands'
+        choices: ['Netherlands', 'France', 'Norway'],
+        answer: 'Netherlands'
     },
     {
         question: "Which planet is closest to the sun?",
-        answers: ['Saturn', 'Mars', 'Mercury'],
-        correctAnswer: 'Mercury'
+        choices: ['Saturn', 'Mars', 'Mercury'],
+        answer: 'Mercury'
     },
     {
         question: "Who was the first president of the United States",
-        answers: ['Washington', 'Lincoln', 'Hamilton'],
-        correctAnswer: 'Washington'
+        choices: ['Washington', 'Lincoln', 'Hamilton'],
+        answer: 'Washington'
     },
 
     {
         question: "What is the capital of Guatemala",
-        answers: ['Guatemala City', 'Antigua', 'Quetzaltenango'],
-        correctAnswer: 'Guatemala City'
+        choices: ['Guatemala City', 'Antigua', 'Quetzaltenango'],
+        answer: 'Guatemala City'
     },
 
-];
-//declare / create variable to hold HTML reference
+]; 
+
+//create variable for the functions of the timer and scores
+var score = 0;
+var currentQuestion = -1;
+var timeLeft = 0;
+var timer;
+
+//start countdown timer once 'start' button is pressed
+function start() {
+
+    timeLeft = 85;
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    timer = setInterval(function() {
+        timeLeft--;
+        document.getElementById("timeLeft").innerHTML = timeLeft;
+        //proceed to end the game function when timer is below 0 at any time
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            endGame(); 
+        }
+    }, 1000); //set pacing of timer to be 1000 millisecond per interval change
+
+    next();
+}
+
+//stop the timer to end the game
+function endGame() {
+    clearInterval(timer);
+
+    //create message within java for game over screen
+    var quizContent = `
+    <h2>Game over!</h2>
+    <h3>You got ` + score +  ` /100!</h3>
+    <h3>That means you got ` + score / 20 +  ` out of 5 questions correct!</h3>
+    <input type="text" id="name" placeholder="Initials"> 
+    <button onclick="setScore()">Set score!</button>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
 
 
-//Event listener for button
-.addeventlistener()
 
-//function to select question     
-function showQuestions(){
-    var quizQuestion = myQuestions [currentQuestionIndex]
-    var questionTitle = document.querySelector('.title')
-    questionTitle.textContent = quizQuestion.question
-
-    // for each question...
-    for(var i=0; i<myQuestions.length; i++);
-    
-};
-
-
-function generateQuiz(){
-    showQuestions();
-    startButton.onclick = function(){
-      
-    }
-
-		
-		/*// first reset the list of answers
-		answers = [];
-
-		// for each available answer to this question...
-		for(letter in questions[i].answers){
-
-			
-
-		// add this question and its answers to the output
-		output.push(
-			'<div class="question">' + questions[i].question + '</div>'
-			+ '<div class="answers">' + answers.join('') + '</div>'
-		);
-	}*/
-
-	// Combine output list and HTML
-	//quizContainer.innerHTML = output.join('');
- //showQuestions(questions, quizContainer);
